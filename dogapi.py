d0fb717b222824aa3a8b42410c2176b574f3816b
Angappan <angappan@decisionminds.com>
2022-07-28 23:09:19 +0530

    Dog API

---
+++ dogapi.py
@@ -0,0 +1,12 @@
+import imp
+import requests
+import json
+import webbrowser
+
+def doggie():
+    f = r"https://random.dog/woof.json"
+    page = requests.get(f)
+    data = json.loads(page.text)
+    webbrowser.open(data["url"])
+
+doggie()
